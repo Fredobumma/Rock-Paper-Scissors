798d0756f1af211e45d3d1c6e0ffca103f74b0f5
Manfred Obumma <fredobumma@gmail.com>
2023-02-18 09:46:07 +0100

    --feat: add routing to application

--- js/script.js
+++ js/script.js
@@ -1,11 +1,112 @@
+import {
+  Home,
+  Login,
+  Register,
+  ResetPassword,
+  NotFound,
+} from "../page-blocks/pages.js";
+
 window.onload = function () {
+  // <----------- IMPLEMENTING ROUTING -------------->
+  const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+  };
+
+  const routes = {
+    "/": Home(),
+    "/login": Login(),
+    "/register": Register(),
+    "/reset-password": ResetPassword(),
+    "/not-found": NotFound(),
+  };
+
+  const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes["/not-found"];
+    document.getElementById("main-page").innerHTML = route;
+  };
+
+  //event listener
+  window.onpopstate = handleLocation;
+  window.route = route;
+  handleLocation();
+
+  // <-------------- IMPLEMENTING NAVBAR ------------->
+  //variables
   const wrapper = document.getElementById("body-wrapper");
   const details = document.getElementsByTagName("details")[0];
   const summary = document.getElementsByTagName("summary")[0];
 
+  //event listener
   wrapper.addEventListener("click", () => {
     wrapper.classList.remove("is-open");
     details.open = false;
   });
   summary.addEventListener("click", () => wrapper.classList.toggle("is-open"));
+
+  // <--------------- IMPLEMENTING GAME HOMEPAGE -------------->
+  //variables
+  const inputOptions = document.getElementsByClassName("input-options");
+  const options = Object.entries(inputOptions);
+  const inputSelection = document.getElementsByClassName("selection");
+  const selections = Object.entries(inputSelection).map(
+    (selection) => selection[1]
+  );
+  const playerOne = selections[0];
+  const playerTwo = selections[1];
+  const result = document.getElementById("result");
+  const newGame = document.getElementById("new-game");
+
+  //functions for operations
+  const comp = (optionValues) =>
+    optionValues[Math.floor(Math.random() * optionValues.length)];
+
+  const gameResult = (optionValues) => {
+    const you = playerOne.innerText;
+    const computer = playerTwo.innerText;
+
+    if (
+      (you === optionValues[0] && computer === optionValues[0]) ||
+      (you === optionValues[1] && computer === optionValues[1]) ||
+      (you === optionValues[2] && computer === optionValues[2])
+    ) {
+      result.style.color = "gray";
+      return "It's a tie!";
+    }
+    if (
+      (you === optionValues[0] && computer === optionValues[1]) ||
+      (you === optionValues[1] && computer === optionValues[2]) ||
+      (you === optionValues[2] && computer === optionValues[0])
+    ) {
+      result.style.color = "red";
+      return "You Lose 😪";
+    }
+    result.style.color = "green";
+    return "You Win 😁";
+  };
+
+  //event listener
+  options.map((option) =>
+    option[1].addEventListener("click", function () {
+      if (playerOne.innerText) return null;
+
+      const optionsArray = options.map((option) => option[1].innerText);
+      playerOne.innerText = option[1].innerText;
+      playerTwo.innerText = comp(optionsArray);
+      result.innerText = gameResult(optionsArray);
+      result.style.display = "block";
+      newGame.style.display = "block";
+    })
+  );
+
+  newGame &&
+    newGame.addEventListener("click", function () {
+      playerOne.innerText = "";
+      playerTwo.innerText = "";
+      result.style.display = "none";
+      newGame.style.display = "none";
+    });
 };
---
+++ page-blocks/pages.js
@@ -0,0 +1,158 @@
+export function Home() {
+  return `<header>
+  <h1 class="heading">Rock, Paper, Scissors</h1>
+</header>
+<section>
+  <p>Pick one</p>
+  <div class="input-selections">
+    <div class="input-options">🧱</div>
+    <div class="input-options">📃</div>
+    <div class="input-options">✂️</div>
+  </div>
+</section>
+<section>
+  <div class="players-container">
+    <div class="selection-container">
+      <p class="player">You</p>
+      <div class="selection"></div>
+    </div>
+    <p class="versus">vs</p>
+    <div class="selection-container">
+      <p class="player">Comp</p>
+      <div class="selection"></div>
+    </div>
+  </div>
+</section>
+<section>
+  <p id="result"></p>
+  <button id="new-game">Play Again</button>
+</section>`;
+}
+
+export function Login() {
+  return `<form class="form">
+  <div class="title">Welcome</div>
+  <div class="subtitle">Login to play!</div>
+  <div class="input-container ic1">
+    <input
+      id="username"
+      class="input"
+      type="text"
+      placeholder=" "
+      autocomplete="username"
+    />
+    <div class="cut"></div>
+    <label for="username" class="placeholder">Username</label>
+  </div>
+  <div class="input-container ic2">
+    <input
+      id="password"
+      class="input"
+      type="password"
+      placeholder=" "
+      autocomplete="current-password"
+    />
+    <div class="cut"></div>
+    <label for="password" class="placeholder">Password</label>
+  </div>
+  <button type="submit" class="submit">submit</button>
+</form>
+<aside>
+  <p><a href="">Forgot Password?</a></p>
+  <p><a href="">Don't have an account? Register!</a></p>
+  <p><a href="">Play as guest...</a></p>
+</aside>`;
+}
+
+export function Register() {
+  return `<form class="form">
+  <div class="title">Welcome</div>
+  <div class="subtitle">Create an account to play!</div>
+  <div class="input-container ic1">
+    <input
+      id="username"
+      class="input"
+      type="text"
+      placeholder=" "
+      autocomplete="username"
+    />
+    <div class="cut"></div>
+    <label for="username" class="placeholder">Username</label>
+  </div>
+  <div class="input-container ic2">
+    <input
+      id="email"
+      class="input"
+      type="text"
+      placeholder=" "
+      autocomplete="email"
+    />
+    <div class="cut cut-short"></div>
+    <label for="email" class="placeholder">Email</label>
+  </div>
+  <div class="input-container ic2">
+    <input
+      id="password"
+      class="input"
+      type="password"
+      placeholder=" "
+      autocomplete="new-password"
+    />
+    <div class="cut"></div>
+    <label for="password" class="placeholder">Password</label>
+  </div>
+  <button type="submit" class="submit">submit</button>
+</form>
+<aside>
+  <p><a href="">Already have an account? Login!</a></p>
+  <p><a href="">Play as guest...</a></p>
+</aside>`;
+}
+
+export function ResetPassword() {
+  return `<form class="form">
+  <div class="subtitle">Reset Password</div>
+  <div class="input-container ic1">
+    <input
+      id="username"
+      class="input"
+      type="text"
+      placeholder=" "
+      autocomplete="username"
+    />
+    <div class="cut"></div>
+    <label for="username" class="placeholder">Username</label>
+  </div>
+  <div class="input-container ic2">
+    <input
+      id="password"
+      class="input"
+      type="password"
+      placeholder=" "
+      autocomplete="new-password"
+    />
+    <div class="cut"></div>
+    <label for="password" class="placeholder">New Password</label>
+  </div>
+  <div class="input-container ic2">
+    <input
+      id="password"
+      class="input"
+      type="password"
+      placeholder=" "
+      autocomplete="new-password"
+    />
+    <div class="cut"></div>
+    <label for="password" class="placeholder">Repeat New Password</label>
+  </div>
+  <button type="submit" class="submit">submit</button>
+</form>
+<aside>
+  <p><a href="">Go back</a></p>
+</aside>`;
+}
+
+export function NotFound() {
+  return `<h1 class="not-found">404</h1>
+<p class="not-found-text">Page Not Found</p>`;
+}
